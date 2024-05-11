ccd9359749ba31940ee8eff0bc674d2ed550877a
Nika Nishnianidze <61516311+Yolosopher@users.noreply.github.com>
2024-05-11 14:43:19 +0000

    fixed header zIndex bug on mobile

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="bg-card shadow-xl">
+    <header className="bg-card shadow-xl relative z-10">
       <div className="container flex h-28 md:justify-center items-center relative">
         <Nav />
         <div className="absolute top-1/2 right-4 -translate-y-1/2">
