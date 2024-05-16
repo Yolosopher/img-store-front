b971cf80d2a7d2700ab83a904d35339eb45eff10
Yolosopher <yolosopher@gmail.com>
2024-05-17 02:26:11 +0400

    Add copyable API origin section on Docs Page

--- src/pages/docs/DocsPage.tsx
+++ src/pages/docs/DocsPage.tsx
@@ -1,5 +1,8 @@
-import { H2 } from "@/components/ui/typography/Heading";
+import { H2, H3, InlineCode, P } from "@/components/ui/typography/Heading";
 import MarkDownItem from "@/components/shared/markdown/MarkDownItem";
+import CopyClipboard from "@/components/shared/clipboard/CopyClipboard";
+import CONFIG from "@/config";
+import { Copy } from "lucide-react";
 
 const DocsPage = () => {
   return (
@@ -7,6 +10,21 @@ const DocsPage = () => {
       <H2 className="uppercase text-center mb-4">
         Instructions to consume API
       </H2>
+
+      <div className="py-4">
+        <H3 className="mb-4 text-primary uppercase pl-2">api origin</H3>
+        <P className="mb-2 pl-2">Use this origin for the following requests:</P>
+        <CopyClipboard
+          as={"div"}
+          className="flex items-center gap-2 cursor-pointer w-max"
+          withoutTooltip
+          title={CONFIG.backend_url}
+          contentToCopy={CONFIG.backend_url}
+        >
+          <InlineCode className="px-2">{CONFIG.backend_url}</InlineCode>
+          <Copy />
+        </CopyClipboard>
+      </div>
       <MarkDownItem
         uniqueId="upload-image"
         src="/docs/upload-image.md"
